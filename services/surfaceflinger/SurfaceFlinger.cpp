LineageOS/android_frameworks_native

SurfaceFlinger: Don't cleanup resources from previous frame
Causes bad lag on some legacy devices.

Change-Id: I89913d214c7377c73bd307696dbf9aac2f9a5c0a

services/surfaceflinger/SurfaceFlinger.cpp
@@ -2344,8 +2344,10 @@ void SurfaceFlinger::postComposition()
     }
     getBE().mLastSwapTime = currentTime;
 
+#ifndef DISABLE_POST_RENDER_CLEANUP
     // Cleanup any outstanding resources due to rendering a prior frame.
     getRenderEngine().cleanupPostRender();
+#endif
 
     {
         std::lock_guard lock(mTexturePoolMutex);

LMODroid/platform_frameworks_native

Signed-off-by: Mohammad Hasan Keramat J <ikeramat80@gmail.com>

services/surfaceflinger/SurfaceFlinger.cpp
@@ -2346,8 +2346,10 @@ void SurfaceFlinger::postComposition()
     }
     getBE().mLastSwapTime = currentTime;
 
+#ifndef DISABLE_POSTRENDER_CLEANUP
     // Cleanup any outstanding resources due to rendering a prior frame.
     getRenderEngine().cleanupPostRender();
+#endif
 
     {
         std::lock_guard lock(mTexturePoolMutex);